a763e38574acd266aa1d3bd1ed941666287291aa
Michell Ayala <starships@outlook.com>
2016-05-25 12:27:34 -0500

    fix: DB_HOST env

--- keys.js
+++ keys.js
@@ -4,6 +4,6 @@ module.exports = {
   account_sid: process.env.TWILIO_ACCOUNT_SID,
   auth_token: process.env.TWILIO_AUTH_TOKEN,
   phone_number: process.env.TWILIO_PHONE_NUMBER,
-  mongo_uri: `mongodb://${DB_HOST}/${process.env.DB_NAME}`
+  mongo_uri: `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`
 };
 
