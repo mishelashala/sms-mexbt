34e6690684965e78702ab9fd6732b5a38a62afd4
Michell Ayala <starships@outlook.com>
2016-05-25 12:25:35 -0500

    update: mongo uri

--- keys.js
+++ keys.js
@@ -4,6 +4,6 @@ module.exports = {
   account_sid: process.env.TWILIO_ACCOUNT_SID,
   auth_token: process.env.TWILIO_AUTH_TOKEN,
   phone_number: process.env.TWILIO_PHONE_NUMBER,
-  mongo_uri: `mongodb://${process.env.MONGOLAB_USER}:${process.env.MONGOLAB_PASS}@ds023902.mlab.com:23902/${process.env.MONGOLAB_DATABASE}`
+  mongo_uri: `mongodb://${DB_HOST}/${process.env.DB_NAME}`
 };
 
