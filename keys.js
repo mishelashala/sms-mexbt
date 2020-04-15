e446f99c9c130eb785b39fc0db235365b63b6727
Michell Ayala <michell.ayala@nuorder.com>
2020-04-14 23:46:00 -0500

    modify(keys): add alphapoint-related env vars

--- keys.js
+++ keys.js
@@ -2,9 +2,9 @@ const { env } = process;
 let config;
 
 const EnvName = {
-  Production: 'production',
-  Staging: 'staging',
-}
+  Production: "production",
+  Staging: "staging",
+};
 
 switch (env.NODE_ENV) {
   case EnvName.Production:
@@ -13,11 +13,17 @@ switch (env.NODE_ENV) {
       auth_token: env.TWILIO_AUTH_TOKEN,
       twilio_phone_number: env.TWILIO_PHONE_NUMBER,
 
+      alphapoint_login_url: env.ALPHAPOINT_LOGIN_URL, // 'https://sim.mexbt.com:8451/ajax/v1/Login'
+      alphapoint_change_verification_level_url:
+        env.ALPHAPOINT_CHANGE_VERIFICATION_URL,
+      alphapoint_admin_username: env.ALPHAPOINT_ADMIN_USERNAME,
+      alphapoint_admin_password: env.ALPHAPOINT_ADMIN_PASSWORD,
+
       db_host: env.PROD_DB_HOST,
       db_port: env.PROD_DB_PORT,
       db_name: env.PROD_DB_NAME,
       db_user: env.PROD_DB_USER,
-      db_pass: env.PROD_DB_PASS
+      db_pass: env.PROD_DB_PASS,
     };
     break;
 
@@ -27,11 +33,17 @@ switch (env.NODE_ENV) {
       auth_token: env.TWILIO_AUTH_TOKEN,
       twilio_phone_number: env.TWILIO_PHONE_NUMBER,
 
+      alphapoint_login_url: env.ALPHAPOINT_LOGIN_URL,
+      alphapoint_change_verification_level_url:
+        env.ALPHAPOINT_CHANGE_VERIFICATION_URL,
+      alphapoint_admin_username: env.ALPHAPOINT_ADMIN_USERNAME,
+      alphapoint_admin_password: env.ALPHAPOINT_ADMIN_PASSWORD,
+
       db_host: env.STAG_DB_HOST,
       db_port: env.STAG_DB_PORT,
       db_name: env.STAG_DB_NAME,
       db_user: env.STAG_DB_USER,
-      db_pass: env.STAG_DB_PASS
+      db_pass: env.STAG_DB_PASS,
     };
     break;
 
@@ -41,6 +53,12 @@ switch (env.NODE_ENV) {
       account_sid: env.TWILIO_ACCOUNT_SID,
       auth_token: env.TWILIO_AUTH_TOKEN,
       twilio_phone_number: env.TWILIO_PHONE_NUMBER,
+
+      alphapoint_login_url: env.ALPHAPOINT_LOGIN_URL,
+      alphapoint_change_verification_level_url:
+        env.ALPHAPOINT_CHANGE_VERIFICATION_URL,
+      alphapoint_admin_username: env.ALPHAPOINT_ADMIN_USERNAME,
+      alphapoint_admin_password: env.ALPHAPOINT_ADMIN_PASSWORD,
 
       phone_number: env.DEV_PHONE_NUMBER,
       phone_region: env.DEV_PHONE_REGION,
@@ -53,7 +71,7 @@ switch (env.NODE_ENV) {
       db_port: env.DEV_DB_PORT,
       db_name: env.DEV_DB_NAME,
       db_user: env.DEV_DB_USER,
-      db_pass: env.DEV_DB_PASS
+      db_pass: env.DEV_DB_PASS,
     };
     break;
 }
--- services/AlphaPointService.js
+++ services/AlphaPointService.js
@@ -1,44 +1,46 @@
-const Axios = require('axios');
+const Axios = require("axios");
+const keys = require("../keys");
 
 export const AlphapointServiceError = {
-  CouldNotAuth: 'Alphapoint Error: Could Not Auth',
-  CouldNotChangeVerificationLevel: 'Alphapoint Error: Coudl Not Change Verification Level'
-}
+  CouldNotAuth: "Alphapoint Error: Could Not Auth",
+  CouldNotChangeVerificationLevel:
+    "Alphapoint Error: Coudl Not Change Verification Level",
+};
 
 /*!
  * Authenticate user and returns session token
  */
 async function auth() {
-  const { data } = await Axios.post('https://sim.mexbt.com:8451/ajax/v1/Login', {
-    adminUserId: 'william',
-    password: 'william123'
+  const { data } = await Axios.post(keys.alphapoint_login_url, {
+    adminUserId: keys.alphapoint_admin_username,
+    password: keys.alphapoint_admin_password,
   });
 
   if (!data.isAccepted) {
-    throw new Error(AlphapointServiceError.CouldNotAuth)
+    throw new Error(AlphapointServiceError.CouldNotAuth);
   }
 
-  return data
+  return data;
 }
 
 /*!
  * Verifies user in Alphapoint's database
  */
 async function verifyUser({ token, email }) {
-  const { data } = await Axios.post('https://sim.mexbt.com:8451/ajax/v1/SetUserVerificationLevel', {
+  const { data } = await Axios.post(keys.alphapoint_change_verification_url, {
     sessionToken: token,
     UserId: email,
-    VerificationLevel: '3'
+    VerificationLevel: "3",
   });
 
   if (!data.isAccepted) {
     throw new Error(AlphapointServiceError.CouldNotChangeVerificationLevel);
   }
 
-  return data
+  return data;
 }
 
 module.exports = {
   auth,
-  verifyUser
-}
+  verifyUser,
+};
