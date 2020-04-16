be226b251a8f88200b1350950ea700fd8540c47d
Michell Ayala <michell.ayala@nuorder.com>
2020-04-16 18:52:35 -0500

    modify(routes/verify): add factory function and decouple services

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
+const { MessagesRouterFactory } = require('./messages')
+const userService = require("../services/UserService");
+const smsService = require('../services/SmsService')
+
+const messages = MessagesRouterFactory({
+  userService,
+  smsService
+})
+
 module.exports = {
-  messages: require("./messages"),
+  messages,
   verify: require("./verify"),
 };
--- routes/messages.js
+++ routes/messages.js
@@ -6,96 +6,101 @@ const Response = require("../utils/response");
 const ClientStatus = require("../utils/client/status");
 const Datadog = require("../utils/datadog");
 const validateUserData = require("../middlewares/validation");
-const UserService = require("../services/UserService");
 
-const MessageRouter = Express.Router();
+function messageRouterFactory({ userService, smsService }) {
+  const MessageRouter = Express.Router();
 
-MessageRouter.post("/", validateUserData, async (req, res) => {
-  try {
-    /*!
-     * #createVerificationData takes the body of the request
-     * and return a formated object
-     */
+  MessageRouter.post("/", validateUserData, async (req, res) => {
+    try {
+      /*!
+       * #createVerificationData takes the body of the request
+       * and return a formated object
+       */
 
-    const data = req.body;
+      const data = req.body;
 
-    /*!
-     * Search one result from the database using
-     * the email as query
-     */
+      /*!
+       * Search one result from the database using
+       * the email as query
+       */
 
-    const user = UserService.findOneByEmailOrCreate(data.email);
+      const user = userService.findOneByEmailOrCreate(data.email);
 
-    data.code = Cuid().slice(0, 8);
+      data.code = Cuid().slice(0, 8);
 
-    /*!
-     * Send the message using twilio library
-     */
+      /*!
+       * Send the message using twilio library
+       */
 
-    await SmsService.sendTextMessage({
-      phone,
-      message: data.code,
-    });
+      await smsService.sendTextMessage({
+        phone,
+        message: data.code,
+      });
 
-    /*!
-     * Report to datadog message sent.
-     * Set code message to user and verified to false
-     */
+      /*!
+       * Report to datadog message sent.
+       * Set code message to user and verified to false
+       */
 
-    Datadog.report("send_message", "message_sent");
+      Datadog.report("send_message", "message_sent");
 
-    /*!
-     * Add verification code to user and flag it as
-     * unverified
-     */
+      /*!
+       * Add verification code to user and flag it as
+       * unverified
+       */
 
-    user.code = data.code;
-    user.verified = false;
+      user.code = data.code;
+      user.verified = false;
 
-    /*!
-     * Save the user, if is not in the data base
-     * upsert (create the user) and then
-     * send the response
-     */
+      /*!
+       * Save the user, if is not in the data base
+       * upsert (create the user) and then
+       * send the response
+       */
 
-    await user.save({ upsert: true });
+      await user.save({ upsert: true });
 
-    /*!
-     * Notify to datadog message was sent
-     */
+      /*!
+       * Notify to datadog message was sent
+       */
 
-    Datadog.report("send_message", "message_database_saved");
+      Datadog.report("send_message", "message_database_saved");
 
-    const responseObject = Response.create({
-      http: HttpStatus.CREATED,
-      client: ClientStatus.MESSAGE_SENT,
-      data: user,
-    });
+      const responseObject = Response.create({
+        http: HttpStatus.CREATED,
+        client: ClientStatus.MESSAGE_SENT,
+        data: user,
+      });
 
-    res.status(HttpStatus.CREATED).json(responseObject);
-  } catch (err) {
-    let http;
+      res.status(HttpStatus.CREATED).json(responseObject);
+    } catch (err) {
+      let http;
 
-    switch (err.message) {
-      case "could_not_send_sms":
-        Datadog.report("send_message", "error_sending_sms");
-        http = HttpStatus.INTERNAL_SERVER_ERROR;
-        client = HttpStatus.MESSAGE_NOT_SENT;
-        break;
-      default:
-        Datadog.report("send_message", "error_database_connection");
-        http = HttpStatus.INTERNAL_SERVER_ERROR;
-        client = HttpStatus.INTERNAL_SERVER_ERROR;
-        break;
+      switch (err.message) {
+        case "could_not_send_sms":
+          Datadog.report("send_message", "error_sending_sms");
+          http = HttpStatus.INTERNAL_SERVER_ERROR;
+          client = HttpStatus.MESSAGE_NOT_SENT;
+          break;
+        default:
+          Datadog.report("send_message", "error_database_connection");
+          http = HttpStatus.INTERNAL_SERVER_ERROR;
+          client = HttpStatus.INTERNAL_SERVER_ERROR;
+          break;
+      }
+
+      const responseObject = Response.create({
+        http,
+        client,
+      });
+
+      res.status(http).json(responseObject);
     }
+  });
 
-    const responseObject = Response.create({
-      http,
-      client,
-    });
+  return MessageRouter;
+}
 
-    res.status(http).json(responseObject);
-  }
-});
-
-module.exports = MessageRouter;
+module.exports = {
+  messageRouterFactory,
+};
